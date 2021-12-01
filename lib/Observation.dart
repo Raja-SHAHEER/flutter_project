484dedbf564aec9874d5ae87e13e6792fe362df0
rajas <raja.shaheer.32@gmail.com>
2021-12-01 17:03:38 +0500

    login Ui completed Tabs included

--- lib/Observation.dart
+++ lib/Observation.dart
@@ -1,3 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:syncfusion_flutter_xlsio/xlsio.dart';
+import 'package:universal_html/html.dart' show AnchorElement;
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'main.dart';
+
 class Observation extends StatefulWidget {
   @override
   State<Observation> createState() => _Observation();
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
-import 'dart:convert';
-import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:open_file/open_file.dart';
-import 'package:path_provider/path_provider.dart';
 
 import 'package:provider/provider.dart';
-import 'package:syncfusion_flutter_xlsio/xlsio.dart';
-
-import 'RetailerInfo.dart';
+import 'TabsLayout.dart';
 import 'auth_services.dart';
 import 'LoginScreen.dart';
 
@@ -35,7 +29,6 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -55,107 +48,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// @override
-// Widget build(BuildContext context) {
-//   return MaterialApp(
-//     title: "JP Survey",
-//     home: DefaultTabController(
-//         length: 2,
-//         initialIndex: 0,
-//         child:Scaffold(
-//           appBar: AppBar(
-//             title: Text("Johan Player"),
-//             bottom: TabBar(
-//               tabs: <Widget>[
-//                 Tab(text: "RetailerInfo",),
-//                 Tab(text: "Observation",)
-//               ],
-//
-//             ),
-//           ),
-//           body: TabBarView(
-//             children: <Widget>[
-//               RetailerInfo(),
-//               Observation(),
-//             ],
-//           ),
-//         )
-//     ),
-//   );
-// }
-class tablayout extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() => _tablayout();
-}
-
-class _tablayout extends State<tablayout>{
-
-  @override
-Widget build(BuildContext context) {
-  return DefaultTabController(
-        length: 2,
-        initialIndex: 0,
-        child:Scaffold(
-          // appBar: PreferredSize(
-          //   preferredSize: Size.fromHeight(50), // here the desired height
-          //   child:AppBar(
-            // centerTitle: true,
-            // title: Text("Johan Player"),
-            body: TabBar(
-              // isScrollable: true,
-              tabs: <Widget>[
-                new Container(
-                    height: 100.0,
-                    child: new Tab(text: "RetailerInfo",)
-                ),
-                new Container(
-                    height: 100.0,
-                    child: new Tab(text: "Observation",)
-                )
-              ],
-
-            ),
-          // ),
-          // ),
-          bottomNavigationBar:TabBarView(
-              children: <Widget>[
-                RetailerInfo(),
-                Observation(),
-              ],
-            ),
-          ),
-        );
-}
-}
-
 class AuthWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = context.watch<User?>();
     if (user != null) {
-      // MaterialApp(
-      //   home: DefaultTabController(
-      //     length: 2,
-      //     initialIndex: 0,
-      //     child: Scaffold(
-      //       appBar: AppBar(
-      //         title: Text("Johan Player"),
-      //         bottom: TabBar(
-      //           tabs: <Widget>[
-      //             Tab(text: "RetailerInfo",),
-      //             Tab(text: "Observation",)
-      //           ],
-      //         ),
-      //       ),
-      //       body: TabBarView(
-      //         children: <Widget>[
-      //           RetailerInfo(),
-      //           Observation(),
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // );
      return tablayout();
       // return RetailerInfo();
     }
